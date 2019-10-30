55f9a20a1059e4cfff59e8585871867a07408b2b
hiya <50945917+heesu40@users.noreply.github.com>
2019-10-30 18:42:29 +0900

    들어가져라!! 얍얍얍! 나의 고생 무너지지말자!

--- release/web/src/main/java/lab/latte/issue/controller/HomeController.java
+++ release/web/src/main/java/lab/latte/issue/controller/HomeController.java
@@ -1,5 +1,7 @@
 package lab.latte.issue.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -8,13 +10,23 @@ import java.util.Properties;
 
 import javax.annotation.Resource;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 import lab.latte.issue.model.TimelineVO;
 import lab.latte.issue.service.IAPIService;
@@ -68,6 +80,69 @@ public class HomeController<T, K, V> {
 	public String pageTest(Locale locale, Model model) {
 		
 		return "page-test";
+	}
+	
+	
+	@Resource(name = "restTemplate")
+	protected RestTemplate restTemplate;
+	
+	@RequestMapping(value = "api/searching" , method = {RequestMethod.GET, RequestMethod.POST})
+	@ResponseBody
+	public Map<String, Object> searchNaver (String[] main , String keyword , String nowTime  ,
+			Model model ) throws  IOException {
+		String clientId = "lXA5GRw7Os5t_Hs1sF28";
+        String clientSecret = "8DC2rlIJdi";
+        
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+
+		try {
+		String apiURL = "https://openapi.naver.com/v1/search/news.json?query="
+    			+ main[0] + "+"
+    			+ main[1] + "+"
+    			+ main[2]
+    			+ "&display=10&start=1&sort=sim";
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		Map<String,String> keyvalue = new HashMap<String, String>();
+		keyvalue.put("X-Naver-Client-Id" , clientId);
+		keyvalue.put( "X-Naver-Client-Secret", clientSecret);
+		
+		headers.setAll(keyvalue);
+		
+		HttpEntity entity = new HttpEntity("parameters" , headers);
+		ResponseEntity response = restTemplate.exchange(apiURL,  HttpMethod.GET , entity , String.class);
+		
+		JSONParser jsonParser = new JSONParser();
+		JSONObject jsonObject = (JSONObject) jsonParser.parse(response.getBody().toString());
+		JSONArray docuArray = (JSONArray)jsonObject.get("items");
+		List originallink = new ArrayList();
+		List description = new ArrayList();
+		List title = new ArrayList();
+		
+		for(int i = 0 ; i <docuArray.size() ; i++) {
+			JSONObject tmp = (JSONObject)docuArray.get(i);
+			title.add((String)tmp.get("title"));
+			originallink.add((String)tmp.get("originallink"));
+			description.add((String)tmp.get("description"));
+			
+		}
+		
+		
+
+		
+		resultMap.put("description", description);
+		resultMap.put("title", title);
+		resultMap.put("originallink", originallink);
+		
+		
+		
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+    	return resultMap;
+       
+        
 	}
 	
 	
